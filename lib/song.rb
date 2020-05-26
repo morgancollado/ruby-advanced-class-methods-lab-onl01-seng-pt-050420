aebe2bb9c897317c8f55cbb754ee49869e3c1e21
Morgan Collado <morgan.collado@gmail.com>
2020-05-26 02:18:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -54,4 +54,13 @@ def self.new_from_filename(filename)
   song 
 end 
 
+def self.create_from_filename(filename)
+  song_data = filename.sub!(/.mp3/,'').split(' - ')
+  
+  song = self.create
+  song.artist_name = song_data[0]
+  song.name = song_data[1]
+  song
+end 
+
 end
